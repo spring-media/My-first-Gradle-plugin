eac52696b02c7b27e3d043a6e946eee88ad6a6c8
skrawczy <sebastian.krawczyk@spring-media.de>
2020-02-25 10:38:14 +0100

    adapt FirstGradlePlugin

--- firstgradleplugin/src/main/java/FirstGradlePlugin.kt
+++ firstgradleplugin/src/main/java/FirstGradlePlugin.kt
@@ -1,17 +1,13 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class FirstGradlePlugin: Plugin<Project> {
+class FirstGradlePlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.afterEvaluate {
-            it.allprojects {
-                val task = project.tasks.create("SayHello") {
-                    println("Hello World!")
-                }
-                task.group = "firstgradleplugin"
-                task.description = "Say hello to the world"
-            }
+        val task = project.tasks.create("SayHello") {
+            println("Hello World!")
         }
+        task.group = "firstgradleplugin"
+        task.description = "Say hello to the world"
     }
 }
